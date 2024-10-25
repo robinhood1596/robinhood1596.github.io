fa76b233c3aea06e9893a6b1e067ba39bb81bd21
robinhood1596 <86231481+robinhood1596@users.noreply.github.com>
2024-10-25 19:42:38 +0200

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -1,2 +1,188 @@
-!function(){"use strict";var e={81192:function(e,t,o){e.exports=o.p+"16090c96ce185fa9f18a.js"}},t={};function o(n){var c=t[n];if(void 0!==c)return c.exports;var r=t[n]={exports:{}};return e[n](r,r.exports,o),r.exports}o.m=e,o.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(e){if("object"==typeof window)return window}}(),o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},function(){var e;o.g.importScripts&&(e=o.g.location+"");var t=o.g.document;if(!e&&t&&(t.currentScript&&"SCRIPT"===t.currentScript.tagName.toUpperCase()&&(e=t.currentScript.src),!e)){var n=t.getElementsByTagName("script");if(n.length)for(var c=n.length-1;c>-1&&(!e||!/^http(s?):/.test(e));)e=n[c--].src}if(!e)throw new Error("Automatic publicPath is not supported in this browser");e=e.replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),o.p=e}(),o.b=document.baseURI||self.location.href,function(){let e=null;async function t(){let t=document.querySelector("video"),o=document.querySelector("canvas");t||(t=document.createElement("video"),document.body.appendChild(t)),o||(o=document.createElement("canvas"),document.body.appendChild(o));const n=document.getElementById("capturePhotoButton");if(n){if(!e)try{const o=await async function(){const e=(await navigator.mediaDevices.enumerateDevices()).filter((e=>"videoinput"===e.kind));if(console.log("Available video devices:",e),e.length>0)return e[0].deviceId;throw new Error("No video devices found")}(),n=await navigator.mediaDevices.getUserMedia({video:{deviceId:{exact:o}}});e=n,t.srcObject=n,t.play()}catch(e){console.error("Error accessing the camera",e)}n.onclick=function(){if(e){const e=1920,n=1080;o.width=e,o.height=n;const c=o.getContext("2d");if(c){c.drawImage(t,0,0,e,n);const r=o.toDataURL("image/png").split(",")[1];console.log("Captured image data URL:",r),function(e){console.log("Inserting image."),Office.context.document.setSelectedDataAsync(e,{coercionType:Office.CoercionType.Image},(function(e){e.status===Office.AsyncResultStatus.Failed?console.error("Error inserting image: "+e.error.message):console.log("Image inserted successfully.")}))}(r),c.clearRect(0,0,o.width,o.height)}else console.error("Failed to get canvas context.")}else console.error("No video stream available")}}else console.error("Capture button not found")}function o(){if(e){e.getTracks().forEach((function(e){e.stop()})),e=null;const t=document.querySelector("video");t&&t.remove();const o=document.querySelector("canvas");o&&o.remove()}}Office.onReady((function(){const e=document.getElementById("capturePhotoButton");e&&e.addEventListener("click",t);const n=document.createElement("button");n.textContent="Stop Camera",document.body.appendChild(n),n.addEventListener("click",o)}))}(),new URL(o(81192),o.b)}();
-//# sourceMappingURL=taskpane.js.map
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var videoStream = null;
+function getCameraDevices() {
+    return __awaiter(this, void 0, void 0, function () {
+        var devices;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, navigator.mediaDevices.enumerateDevices()];
+                case 1:
+                    devices = _a.sent();
+                    return [2 /*return*/, devices.filter(function (device) { return device.kind === 'videoinput'; })];
+            }
+        });
+    });
+}
+function populateCameraDropdown() {
+    return __awaiter(this, void 0, void 0, function () {
+        var cameraDropdown, videoDevices;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    cameraDropdown = document.getElementById("cameraDropdown");
+                    if (!cameraDropdown) {
+                        console.error("Camera dropdown not found");
+                        return [2 /*return*/];
+                    }
+                    return [4 /*yield*/, getCameraDevices()];
+                case 1:
+                    videoDevices = _a.sent();
+                    videoDevices.forEach(function (device) {
+                        var option = document.createElement("option");
+                        option.value = device.deviceId;
+                        option.text = device.label || "Camera ".concat(cameraDropdown.options.length + 1);
+                        cameraDropdown.appendChild(option);
+                    });
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function insertImageToSlide(imageDataUrl) {
+    console.log("Inserting image.");
+    Office.context.document.setSelectedDataAsync(imageDataUrl, { coercionType: Office.CoercionType.Image }, function (asyncResult) {
+        if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+            console.error("Error inserting image: " + asyncResult.error.message);
+        }
+        else {
+            console.log("Image inserted successfully.");
+        }
+    });
+}
+function capturePhoto() {
+    return __awaiter(this, void 0, void 0, function () {
+        var videoElement, canvasElement, captureButton, cameraDropdown, selectedDeviceId, stream, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    videoElement = document.querySelector("video");
+                    canvasElement = document.querySelector("canvas");
+                    if (!videoElement) {
+                        videoElement = document.createElement("video");
+                        document.body.appendChild(videoElement);
+                    }
+                    if (!canvasElement) {
+                        canvasElement = document.createElement("canvas");
+                        document.body.appendChild(canvasElement);
+                    }
+                    captureButton = document.getElementById("capturePhotoButton");
+                    if (!captureButton) {
+                        console.error("Capture button not found");
+                        return [2 /*return*/];
+                    }
+                    if (!!videoStream) return [3 /*break*/, 4];
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
+                    cameraDropdown = document.getElementById("cameraDropdown");
+                    if (!cameraDropdown) {
+                        console.error("Camera dropdown not found");
+                        return [2 /*return*/];
+                    }
+                    selectedDeviceId = cameraDropdown.value;
+                    return [4 /*yield*/, navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: selectedDeviceId } } })];
+                case 2:
+                    stream = _a.sent();
+                    videoStream = stream;
+                    videoElement.srcObject = stream;
+                    videoElement.play();
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _a.sent();
+                    console.error("Error accessing the camera", error_1);
+                    return [3 /*break*/, 4];
+                case 4:
+                    captureButton.onclick = function () {
+                        if (videoStream) {
+                            var desiredWidth = 1920; // Example width for high resolution
+                            var desiredHeight = 1080; // Example height for high resolution
+                            if (canvasElement) {
+                                canvasElement.width = desiredWidth;
+                                canvasElement.height = desiredHeight;
+                                var context = canvasElement.getContext("2d");
+                                if (context && videoElement) {
+                                    context.drawImage(videoElement, 0, 0, desiredWidth, desiredHeight);
+                                    var imageDataUrl = canvasElement.toDataURL("image/png").split(',')[1];
+                                    console.log("Captured image data URL:", imageDataUrl);
+                                    insertImageToSlide(imageDataUrl);
+                                    context.clearRect(0, 0, canvasElement.width, canvasElement.height);
+                                }
+                                else {
+                                    console.error("Failed to get canvas context or video element.");
+                                }
+                            }
+                        }
+                        else {
+                            console.error("No video stream available");
+                        }
+                    };
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function stopCamera() {
+    if (videoStream) {
+        videoStream.getTracks().forEach(function (track) { return track.stop(); });
+        videoStream = null;
+        var videoElement = document.querySelector("video");
+        if (videoElement) {
+            videoElement.remove();
+        }
+        var canvasElement = document.querySelector("canvas");
+        if (canvasElement) {
+            canvasElement.remove();
+        }
+    }
+}
+Office.onReady(function () {
+    var captureButton = document.getElementById("capturePhotoButton");
+    if (captureButton) {
+        captureButton.addEventListener("click", capturePhoto);
+    }
+    var stopButton = document.createElement("button");
+    stopButton.textContent = "Stop Camera";
+    document.body.appendChild(stopButton);
+    stopButton.addEventListener("click", stopCamera);
+    // Create and populate the camera dropdown
+    var cameraDropdown = document.createElement("select");
+    cameraDropdown.id = "cameraDropdown";
+    document.body.appendChild(cameraDropdown);
+    populateCameraDropdown();
+});
